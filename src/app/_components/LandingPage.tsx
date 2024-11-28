a4d24b6d142d805ed5611864ecf0828fb7d77f35
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-11-28 23:51:02 +0100

    removing boilerplate, adding landingpage

---
+++ src/app/_components/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+function LandingPage({}: Props) {
+  return (
+    <div className="flex flex-col">
+      <h1>go2spot</h1>
+      <p>find cool locations in your destination area</p>
+    </div>
+  );
+}
+
+export default LandingPage;
