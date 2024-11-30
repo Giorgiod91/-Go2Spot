5670a42ab258e95f5ac4defd497cd49dac505d1b
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-11-30 13:19:41 +0100

    adding navbar adding imagecard to render images from the spots to go also added daisyui styles

---
+++ src/app/_components/ImageCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function ImageCard({}: Props) {
+  return (
+    <div className="flex">
+      <img src="null" alt="image" />
+    </div>
+  );
+}
+
+export default ImageCard;
--- src/app/_components/LandingPage.tsx
+++ src/app/_components/LandingPage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 
 type Props = {};
 
 function LandingPage({}: Props) {
   return (
-    <div className="flex flex-col">
+    <div className="w-7xl flex flex-col">
       <h1 className="text-7xl font-black">Find places with go2spot</h1>
-      <p>find cool locations in your destination area</p>
+      <p className="">find cool locations in your destination area</p>
+
+      <div className="flex flex-row">
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </div>
     </div>
   );
 }
