083ed801565d0d7d1e406bd11d3e5ec7a0b525e8
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-11-30 13:29:00 +0100

    added hard coded data for the imagecards for now, adding input field

---
+++ src/app/_components/HardCodedData.tsx
@@ -0,0 +1,22 @@
+import { describe } from "node:test";
+
+export const Barcelona = [
+  {
+    title: "Sagrada Familia",
+    img: "",
+    description: "Good looking church",
+    location: "Barcelona, Spain",
+  },
+  {
+    title: "Park Güell",
+    img: "",
+    description: "Colorful park with amazing architecture by Gaudí",
+    location: "Barcelona, Spain",
+  },
+  {
+    title: "Casa Batlló",
+    img: "",
+    description: "Iconic modernist building designed by Gaudí",
+    location: "Barcelona, Spain",
+  },
+];
--- src/app/_components/LandingPage.tsx
+++ src/app/_components/LandingPage.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ImageCard from "./ImageCard";
 
 type Props = {};
 
 function LandingPage({}: Props) {
+  const [input, setInput] = useState("");
   return (
     <div className="w-7xl flex flex-col">
       <h1 className="text-7xl font-black">Find places with go2spot</h1>
       <p className="">find cool locations in your destination area</p>
 
-      <div className="flex flex-row">
-        <ImageCard />
+      <div className="flex flex-row space-x-2 p-2">
         <ImageCard />
         <ImageCard />
         <ImageCard />
