c9e2b417f4b00c0d628b47ca3e94b2be6531dd44
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-12-04 01:40:18 +0100

    added picture to the card with for now hardcoded images be able to use the src as props

--- src/app/_components/HardCodedData.tsx
+++ src/app/_components/HardCodedData.tsx
@@ -2,20 +2,23 @@ import { describe } from "node:test";
 
 export const Barcelona = [
   {
+    id: 1,
     title: "Sagrada Familia",
-    img: "",
+    img: "./sagrada.png",
     description: "Good looking church",
     location: "Barcelona, Spain",
   },
   {
+    id: 2,
     title: "Park Güell",
-    img: "",
+    img: "./park.png",
     description: "Colorful park with amazing architecture by Gaudí",
     location: "Barcelona, Spain",
   },
   {
+    id: 3,
     title: "Casa Batlló",
-    img: "",
+    img: "./Casa.png",
     description: "Iconic modernist building designed by Gaudí",
     location: "Barcelona, Spain",
   },
--- src/app/_components/LandingPage.tsx
+++ src/app/_components/LandingPage.tsx
@@ -1,20 +1,29 @@
 "use client";
+
 import React, { useState } from "react";
 import ImageCard from "./ImageCard";
+import { Barcelona } from "./HardCodedData";
 
 type Props = {};
 
 function LandingPage({}: Props) {
   const [input, setInput] = useState("");
+
   return (
-    <div className="w-7xl flex flex-col">
+    <div className="w-7xl flex flex-col items-center">
       <h1 className="text-7xl font-black">Find places with go2spot</h1>
-      <p className="">find cool locations in your destination area</p>
+      <p className="">Find cool locations in your destination area</p>
 
       <div className="flex flex-row space-x-2 p-2">
-        <ImageCard />
-        <ImageCard />
-        <ImageCard />
+        {Barcelona.map((place) => (
+          <ImageCard
+            key={place.id}
+            title={place.title}
+            img={place.img}
+            description={place.description}
+            location={place.location}
+          />
+        ))}
       </div>
     </div>
   );
