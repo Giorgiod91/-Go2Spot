debc4aceca5e4ed942fb18d69a95c45dfbf7b720
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-12-04 01:40:18 +0100

    added picture to the card with for now hardcoded images be able to use the src as props

--- src/app/_components/ImageCard.tsx
+++ src/app/_components/ImageCard.tsx
@@ -1,22 +1,21 @@
-import React from "react";
+type ImageCardProps = {
+  title: string;
+  img: string;
+  description: string;
+  location: string;
+};
 
-type Props = {};
-
-function ImageCard({}: Props) {
+function ImageCard({ title, img, description, location }: ImageCardProps) {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl hover:skew-y-3 hover:border-4 hover:border-dotted">
       <figure className="px-10 pt-10">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <img src={img} alt={title} className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{title}</h2>
+        <p>{description}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">Visit {location}</button>
         </div>
       </div>
     </div>
