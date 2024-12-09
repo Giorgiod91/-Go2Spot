6e6ea22e363ff28e94dff067137bbab0619e6816
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-12-09 02:19:35 +0100

    adding inputfield with function that tracks state of the picked one from the select option from the most viewed destinations

--- src/app/_components/InputField.tsx
+++ src/app/_components/InputField.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 type Props = {};
 
 function InputField({}: Props) {
+  const [destination, setDestination] = useState("");
+  const [input, setInput] = useState("");
+
+  // change state for the select part
+  const handleDestinationChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setDestination(event.target.value);
+  };
   return (
     <div className="h-35 w-[340px] border">
       <input
@@ -10,6 +20,19 @@ function InputField({}: Props) {
         type="Your Destination"
         placeholder="Your Destination"
       />
+      <select
+        onChange={handleDestinationChange}
+        value={destination}
+        name=""
+        id=""
+      >
+        <option value="" disabled>
+          Check out most viewed ones
+        </option>
+        <option value="Barcelona">Barcelona</option>
+        <option value="London">London</option>
+        <option value="Berlin">Berlin</option>
+      </select>
     </div>
   );
 }
