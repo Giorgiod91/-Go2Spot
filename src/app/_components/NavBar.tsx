a64000e659d9b201654421c5ded6b5e79342ed2c
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-12-12 15:41:56 +0100

    added google maps with an api also had to add a MapclientComponent to bypass next.js server side that i can use maps provider on the client side

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 function NavBar({}: Props) {
   return (
-    <div className="mx-auto flex max-w-7xl items-center justify-between px-4">
+    <div className="mx-auto flex w-full items-center justify-between bg-primary px-4">
       <div className="flex items-center">
-        <h2>go2spot</h2>
+        <h2 className="text-2xl">go2spot</h2>
       </div>
       <div className="flex items-center space-x-4">
         <h2>Info</h2>
