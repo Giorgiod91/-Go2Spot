3548747e95d65e20baa892ce573d23aab86bd11a
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2024-12-12 16:03:17 +0100

    integrated Maps JavaScript API from google cloud diplaying the map

--- src/app/_components/ShowCase.tsx
+++ src/app/_components/ShowCase.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+import { Map } from "@vis.gl/react-google-maps";
+import { MapCameraChangedEvent } from "@vis.gl/react-google-maps";
 import React from "react";
 
 type Props = {};
 
 function ShowCase({}: Props) {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <h1 className="text-7xl font-extrabold">OverView</h1>
-      <div className="max-w-7xl"></div>
+      <div className="max-w-7xl" style={{ width: "100%", height: "500px" }}>
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom,
+            )
+          }
+        />
+      </div>
     </div>
   );
 }
